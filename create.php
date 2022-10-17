38eb7d16a20b29d59dff74c673659d19c353f228
Andrew-Keller <112123995+Andrew-Keller@users.noreply.github.com>
2022-10-16 22:16:28 -0400

    Add files via upload

---
+++ create.php
@@ -0,0 +1,141 @@
+
+<?php
+	#$quote = $_GET['quote'];
+	require_once('csv_util.php');
+	$authorarray = csvFileToArray('authors.csv');
+	$quotearray = csvFileToArray('quotes.csv');
+?>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title><?='Great Quotes - ASE 230 Project'?></title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container px-4 px-lg-5">
+              
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Site Map
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="index.php">Quotes</a></li>
+                        <li class="nav-item"><a class="nav-link" href="create.php">Create</a></li>
+                        <li class="nav-item"><a class="nav-link" href="modify.php">Modify</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <section class="about-section text-center" id="about">
+        </section>
+        <section class="projects-section bg-light" id="projects">
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+               	<div class="col-lg-6">
+					<div class="bg-black text-center h-100 project">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-left">
+                            		<form method="get" action="create.php">
+                            		<div class="form-group">
+    									<label for="exampleFormControlInput1">Quote Entry</label>
+    									<input type="text" class="form-control" name="quote" placeholder="Enter the quote here:">
+ 									</div>
+                            		<div class="form-group">
+    									<label for="exampleFormControlSelect1">Authors</label>
+    									<select class="form-control" name="author">
+    										<?php
+    										for($i=1;$i<count($authorarray);$i++){
+    										?>
+    										<option><?= $authorarray[$i][0].' '.$authorarray[$i][1] ?></option>
+    										<?php
+    										}
+    										?>
+    									</select>
+  									</div>
+  									<br></br>
+  									<button type="submit" class="btn btn-primary">Add Quote</button>
+  								</form>
+                                <hr class="d-none d-lg-block mb-0 ms-0" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br></br>
+   
+    </div>
+        </section>
+        <!-- Signup-->
+        
+        <!-- Contact-->
+        <section class="contact-section bg-black">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Address</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">Cincinnati, Ohio</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-envelope text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">School Email</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50"><a href="#!">kellera11@mymail.nku.edu</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Email</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">andrewkeller256@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="social d-flex justify-content-center">
+                    <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                    <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                    <a class="mx-2" href="#!"><i class="fab fa-github"></i></a>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Your Website 2022</div></footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
+
+
+#the file displays a form with a text field where users can type the quote and a select box #that displays all the available authors
+#as the form gets submitted, a new quote is added to quotes.csv
